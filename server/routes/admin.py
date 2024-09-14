782a3e5be59c60c2644ab80454c29354fd7cba40
luddite478 <luddite478@gmail.com>
2024-09-15 00:49:40 +0400

    rm babel

--- server/routes/admin.py
+++ server/routes/admin.py
@@ -7,23 +7,12 @@ from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from flask import Blueprint, render_template
 from scripts.get_posts import get_posts
-from babel.dates import format_date
-from flask_babel import Babel
 
 # logging.basicConfig(level=logging.DEBUG)
 # print = logging.debug/
 logging.getLogger('pymongo').setLevel(logging.WARNING)
 
 admin = Blueprint('admin', __name__)
-
-# Initialize Babel
-app = Flask(__name__)
-babel = Babel(app)
-
-@babel.localeselector
-def get_locale():
-    # You can use request.accept_languages to get the best match for the user's language
-    return request.accept_languages.best_match(['en', 'es', 'fr', 'de', 'zh'])
 
 @admin.route('/')
 def admin_page():
@@ -37,7 +26,7 @@ def admin_page():
         try:
             post_data = {
                 "id": str(post['_id']) if '_id' in post else '',
-                "date": format_date(post['date'], format='long', locale=get_locale()) if 'date' in post else '',
+                "date": post['date'].strftime('%B %d, %Y') if 'date' in post else '',
                 "blocks": post['blocks'] if 'blocks' in post else [],
                 "title": post['title'] if 'title' in post else ''
             }
